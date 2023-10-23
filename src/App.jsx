4832808ad02acd4f9224882e5fb71b2820d269ef
jerodrisan <chukks@gmail.com>
2023-10-24 00:32:07 +0200

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,46 @@
-//import { useState, useEffect } from 'react'
-//import axios from 'axios'
-import { useEffect } from 'react'
-import {useField} from './hooks/Usefield'
-import useResource from './hooks/Useresource'
-
 //instalamos json server como dependencia de desarrollo : npm install json-server --save-dev
 //añadimos en el package.json, scripts  : "server": "json-server -p3005 --watch db.json" 
+import axios from "axios"
+import React from 'react'
 
-
-const App = () => {
-  const content = useField('text')
-  const name = useField('text')
-  const number = useField('text')
-
-  const [notes, noteService] = useResource('http://localhost:3005/notes')
-  const [persons, personService] = useResource('http://localhost:3005/persons')
-
-  useEffect(()=>{
-    personService.getAll()
-    noteService.getAll() 
-  },[])
-
-  const handleNoteSubmit = (event) => {
-    event.preventDefault()
-    noteService.create({ content: content.value })
-  }
- 
-  const handlePersonSubmit = (event) => {
-    event.preventDefault()
-    personService.create({ name: name.value, number: number.value})
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      anecdotes: [],
+      current: 0
+    }
   }
 
-  return (
-    <div>
-      <h2>notes</h2>
-      <form onSubmit={handleNoteSubmit}>
-        <input {...content} />
-        <button>create</button>
-      </form>
-      {notes.map(n => <p key={n.id}>{n.content}</p>)}
+  //the lifecycle method componentDidMount,  is executed once right after the first time a component renders
+  //Calling the method setState always triggers the rerender of the Class Component, i.e. calling the method render
+  componentDidMount = () => {
+    axios.get('http://localhost:3005/anecdotes').then(response => {
+      this.setState({ anecdotes: response.data })
+    })
+  }
 
-      <h2>persons</h2>
-      <form onSubmit={handlePersonSubmit}>
-        name <input {...name} /> <br/>
-        number <input {...number} />
-        <button>create</button>
-      </form>
-      { persons.map(n => <p key={n.id}>{n.name} {n.number}</p>)}
-    </div>
-  )
+  handleClick = () => {
+    const current = Math.floor(Math.random() *this.state.anecdotes.length)
+    this.setState({ current: current})
+  }
+
+
+  render() {
+    if (this.state.anecdotes.length === 0) {
+      return <div>no anecdotes...</div>
+    }
+
+    return (
+      <div>
+        <h1>anecdote of the day</h1>
+        <div>
+          {this.state.anecdotes[this.state.current].content}
+        </div>
+        <button onClick ={this.handleClick}>next</button>
+      </div>
+    )
+  }
 }
 
 export default App
